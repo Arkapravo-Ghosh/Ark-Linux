606343ee006ea3e5d9e9c7a9178855210082d69f
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-09-16 16:50:56 +0530

    feat: added custom neofetch

--- arklinux/profiledef.sh
+++ arklinux/profiledef.sh
@@ -26,4 +26,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/neofetch"]="0:0:755"
 )
